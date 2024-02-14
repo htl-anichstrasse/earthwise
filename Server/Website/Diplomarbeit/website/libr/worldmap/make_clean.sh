40d0e42110eb9dbc0887571ab5d47a2f5c6731cf
SimmaJulian <simma.julian@gmail.com>
2024-02-14 18:54:25 +0100

    M

--- Server/Website/Diplomarbeit/website/libr/worldmap/make_clean.sh
+++
@@ -1,8 +0,0 @@
-echo "Cleaning previous builds first.."
-rm -rf dist
-rm -rf build
-rm -rf worldmap.egg-info
-rm -rf worldmap//data//SVG_MAPS
-rm -rf worldmap//data//SVG_MAPS.zip
-rm -rf worldmap//data//citycode.txt
-rm -rf *.svg
