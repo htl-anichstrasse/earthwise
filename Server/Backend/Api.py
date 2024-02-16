fb3555b4299013426278f379f6cff626d0c07928
youngnight91 <david.mark.privat@gmail.com>
2024-02-16 13:16:47 +0100

    Kommentiert

--- Server/Backend/Api.py
+++ Server/Backend/Api.py
@@ -6,37 +6,32 @@ import ArrangeDataForApi as ad
 app = FastAPI()
 
 # ----- QUIZ -----
-##TODO DA FEHLT A NO WAS
+##TODO KOMPLETT ABGESCHLOSSEN
 
 # GET QUIZ OVERVIEW (to display all quizzes in an overview)
 @app.get("/getquizoverview") # /getquizoverview
 def getquizoverview():
     return ad.get_quiz_overview() # {"quiz_data": [[quiz_id, name, quiz_type], ...]}
-##TODO GEHT
 
 # GET QUIZ BY ID (to get all the data from a quiz by id)
 @app.get("/getquizbyid/{id}") # /getquizbyid/id
 def getquiz(id: int):
     return ad.get_quiz_by_id(id) # {"quiz_id": quiz_id, "quiz_name": name, "description": description, "quiz_type":quiz_type, "country_data": country_data}
-##TODO GEHT
 
 # GET ALL QUIZ DATA (for the offline version of the app to save the quizzes in a text document)
 @app.get("/getallquizdata") # /getallquizdata
 def getallquizdata():
     return ad.get_all_quiz_data() # [{"quiz_id": quiz_id,"quiz_name":name,"description": description,"quiz_type":quiz_type,"country_data":["cca2","cca2", ...]}, ... ]
-##TODO GEHT
 
 # GET ALL alternativeE SPELLINGS (for the offline version of the app to save the possible spellings in a text document)
 @app.get("/getallalternativeespellings") # /getallalternativeespellings
 def getallalternativeespellings():
     return ad.get_all_alternative_spellings() # {"cca2":["alternativee_spelling", ...], ...}
-##TODO GEHT
 
 # GET alternativeE SPELLINGS BY CCA2 (to get the alternative spelling to a cca2)
 @app.get("/getalternativespellingsbycca2/{cca2}") # /getalternativespellingsbycca2/cca2
 def getalternativespellingsbycca2(cca2: str):
     return ad.get_alternative_spellings_by_cca2(cca2) # ["alternative_spelling", ...] OR {"message_type": message_type,"message": message}
-##TODO GEHT
 
 # ----- USER -----
 ##TODO KOMPLETT ABGESCHLOSSEN
@@ -77,25 +72,22 @@ def increaselevel(email: str, levelincrease: int):
     return ad.increase_level(email, levelincrease) # {"message_type": message_type,"message": message}
 
 # ----- SCORE -----
-##TODO GEHT
+##TODO KOMPLETT ABGESCHLOSSEN
 
 # SET SCORE - API
 @app.get("/setscore/{email}/{quizid}/{score}/{achivablescore}/{neededtime}") # /setscore/email/quizid/score/achivablescore/neededtime
 def setscore(email: str, quizid: int, score: int, achivablescore: int, neededtime: int):
     return ad.set_score(email, quizid, score, achivablescore, neededtime) # {"message_type": message_type,"message": message}
-##TODO GEHT
 
 # GET SCORE - API
 @app.get("/getscore/{email}/{quizid}") # /getscore/email/quizid
 def getscore(email: str, quizid: int):
     return ad.get_score(email, quizid) # [email, quiz_id, score, achivable_score, needed_time] OR {"message_type": message_type,"message": message}
-##TODO GEHT
 
 # GET ALL SCORES - API
 @app.get("/getallscores/{email}") # /getallscores/email
 def getallscores(email: str):
     return ad.get_all_scores_by_email(email) # [[email, quiz_id, score, achivable_score, needed_time], ...]
-##TODO GEHT
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8080)
--- Server/Backend/ArrangeDataForApi.py
+++ Server/Backend/ArrangeDataForApi.py
@@ -1,40 +1,46 @@
 import json
 import ast
-
-from annotated_types import UpperCase
 import DbConnector as dbc
 
 # ----- QUIZ -----
+##TODO KOMPLETT ABGESCHLOSSEN
 
-# GET QUIZ OVERVIEW - FILTER UND ARRANGE JSON STRING
+# GET QUIZ OVERVIEW
 def get_quiz_overview():
+    # gets all ids and names of the quizzes from the database and returns them
     quiz_data = dbc.get_quiz_overview()
     json_string = {
         "quiz_data": quiz_data
     }
     return json_string
-##TODO GEHT
 
-# GET QUIZ BY ID - FILTER AND ARRAGE JSON STRING
+# GET QUIZ BY ID
 def get_quiz_by_id(id):
+    #gets all the data from the quiz with this id
     quiz_data = dbc.get_quiz_by_id(id)
     row = quiz_data[0]
-    json_string = select_quiz_and_filter(row)
+    # filters according to the quiz type and returns corresponding data and returns them
+    json_string = filter_quiz(row)
     return json_string
-##TODO GEHT
 
 # GET ALL QUIZ DATA
 def get_all_quiz_data():
+    # gets all data for each quiz from the database
     quiz_data = dbc.get_all_quiz_data()
     json_string = []
+    # goes through each quiz individually
     for quiz in quiz_data:
-        quiz_json = select_quiz_and_filter(quiz)
+        # filters according to the quiz type and returns corresponding data
+        quiz_json = filter_quiz(quiz)
         json_string.append(quiz_json)
+    # returns all quiz data
     return json_string
-##TODO GEHT
 
-def select_quiz_and_filter(quiz):
+# FILTER QUIZ
+def filter_quiz(quiz):
+    # splits the quiz into its individual values
     quiz_id, name, description, quiz_type, select_statement = quiz
+    # if its a map or a flag quiz the select statement is executed and is returned in a json
     if quiz_type == "mapquiz" or quiz_type == "flagquiz":
         country_data = dbc.get_countries_by_quiz(select_statement)
         country_data_filtered = []
@@ -47,6 +53,7 @@ def select_quiz_and_filter(quiz):
             "quiz_type": quiz_type,
             "country_data": country_data_filtered
         }
+    # if it is a table quiz, more values ​​are expected and filtered accordingly
     elif quiz_type == "tablequiz":
         country_data = dbc.get_countries_by_quiz(select_statement)
         country_data_filtered = []
@@ -62,6 +69,7 @@ def select_quiz_and_filter(quiz):
             "quiz_type": quiz_type,
             "country_data": country_data_filtered
         } 
+    # if it is a neighboring country quiz, the data is already in the select_statement because it could not be summarized in a select statement
     elif quiz_type == "neighboringcountries":
         quiz_json = {
             "quiz_id": quiz_id,
@@ -70,43 +78,52 @@ def select_quiz_and_filter(quiz):
             "quiz_type": quiz_type,
             "country_data": select_statement
         } 
+    # at the end the corresponding json is returned
     return quiz_json
 
 # GET ALL ALTERNATIVE SPELLINGS
 def get_all_alternative_spellings():
+    # gets all the names and cca2s of the countries from the database
     country_data = dbc.get_countries_by_quiz("select cca2, name from country where independent = true")
     return_value = {}
+    # each country is examined individually and checked to see whether alternative spellings exist
     for country in country_data:
         alt_spell = dbc.get_alternative_spellings_to_cca2(country[0])
         if alt_spell != []:
+            # if this is the case, they will be added to the return value
             alt_spell_list = [country[1]]
             for i in alt_spell:
                 alt_spell_list.append(i[0])
             return_value.update({country[0]: alt_spell_list})
         else:
+            # if not, only the name of the country is added
             return_value.update({country[0]: [country[1]]})
     return return_value
-##TODO GEHT
 
 # GET ALTERNATIVE SPELLINGS BY CCA2
 def get_alternative_spellings_by_cca2(cca2):
+    # checks whether this cca2 exists
     cca2_exists = check_if_cca2_exists(cca2)
     if cca2_exists["message_type"] == "Success":
+        # if this is the case, the corresponding name is fetched from the database and the corresponding alternative spellings
         country_data = dbc.get_countries_by_quiz("select name from country where independent = true and cca2 = \'" + cca2 + "\'")
         alt_spell = dbc.get_alternative_spellings_to_cca2(cca2)
+        # it is then compiled into a return value
         name = country_data[0]
         alt_spell_list = [name[0]]
         if alt_spell != []:
             for i in alt_spell[0]:
                 alt_spell_list.append(i)
         return alt_spell_list
+    # if cca2 does not exist an error message is returned
     return cca2_exists
-##TODO GEHT
             
-        
+# CHECH IF CCA2 EXISTS    
 def check_if_cca2_exists(cca2):
+    # gets all cca2s from the database
     country_data = dbc.get_countries_by_quiz("select cca2 from country where independent = true")
     for i in country_data:
+        # go through them all and see if any of them match what was handed over and returns a corresponding message
         if i[0] == cca2:
             json_string = {
                 "message_type": "Success",
@@ -118,11 +135,13 @@ def check_if_cca2_exists(cca2):
         "message": "No existing country with this cca2."
     }
     return json_string
-##TODO GEHT
 
+# CHECK IF QUIZ EXISTS
 def check_if_quiz_exists(quiz_id):
+    # gets all quizzes from the database
     quiz_data = dbc.get_all_quiz_data()
     for quiz in quiz_data:
+        # go through them all and see if any of them match what was handed over and returns a corresponding message
         if quiz_id == quiz[0]:
             json_string = {
                 "message_type": "Success",
@@ -134,7 +153,6 @@ def check_if_quiz_exists(quiz_id):
         "message": "No existing quiz with this id."
     }
     return json_string
-##TODO GEHT
 
 # ----- USER -----
 ##TODO KOMPLETT ABGESCHLOSSEN
@@ -327,17 +345,22 @@ def increase_level(email, levelincrease):
     return email_exists
 
 # ----- SCORE -----
+##TODO KOMPLETT ABGESCHLOSSEN
 
 # CHECK IF SCORE EXISTS
 def check_if_score_exists(email, quiz_id):
+    # gets all scores from the database
     score_data = dbc.get_all_score_data()
+    # goes through each score individually and see if the email and id matches
     for score in score_data:
         if score[0] == email and score[1] == quiz_id:
+            # returns a success message if this is the case
             json_string = {
                 "message_type": "Success",
                 "message": ""
             }
             return json_string
+    # returns a error message if this is not the case
     json_string = {
         "message_type": "Error",
         "message": "No existing score with this email and quiz_id."
@@ -346,14 +369,20 @@ def check_if_score_exists(email, quiz_id):
 
 # SET SCORE
 def set_score(email, quiz_id, score, achivable_score, needed_time):
+    # checks whether the email exists
     email_exists = check_if_email_exists(email)
     if email_exists["message_type"] == "Success":
+        # checks whether the quiz exists
         quiz_exists = check_if_quiz_exists(quiz_id)
         if quiz_exists["message_type"] == "Success":
+            # checks whether a score with this email and this id already exists
             score_exists = check_if_score_exists(email, quiz_id)
             if score_exists["message_type"] == "Success":
-                score_db = get_score(email, quiz_id) #TODO GEHT NO NIT
+                # of this is the case, the score is fetched from the database
+                score_db = get_score(email, quiz_id)
+                # checks whether the new score is highrt
                 if score > score_db[2]:
+                    # if this is the case, it will be entered in the database and a corresponding message is returned
                     dbc.delete_score(email, quiz_id)
                     deleted_score_exists = check_if_score_exists(email, quiz_id)
                     if deleted_score_exists["message_type"] == "Success":
@@ -369,8 +398,11 @@ def set_score(email, quiz_id, score, achivable_score, needed_time):
                         "message": "The score has been updated."
                         }
                         return json_string
+                # if the new score is not higher, it is checked whether it is identical
                 elif score == score_db[2]:
+                    # if this is the case, it will be checked whether the new time was faster
                     if needed_time < score_db[4]:
+                        # if this is the case, the score is updated and a corresponding message is returned
                         dbc.delete_score(email, quiz_id)
                         deleted_score_exists = check_if_score_exists(email, quiz_id)
                         if deleted_score_exists["message_type"] == "Success":
@@ -387,18 +419,21 @@ def set_score(email, quiz_id, score, achivable_score, needed_time):
                             }
                             return json_string
                     elif needed_time >= score_db[4]:
+                        # if this is not the case, a corresponding message is returned
                         json_string = {
                             "message_type": "Error",
                             "message": "There is already an entry in the database with a shorter time."
                         }
                         return json_string
                 elif score < score_db[2]:
+                    # if the new score is lower or the email or quiz does not exist, an error message will also be returned
                     json_string = {
                         "message_type": "Error",
                         "message": "There is already an entry in the database with a higher score."
                     }
                     return json_string
             else:
+                # if the score does not yet exist, a new one is created
                 create_score = dbc.create_new_score(email, quiz_id, score, achivable_score, needed_time)
                 return create_score
         return quiz_exists
@@ -406,14 +441,19 @@ def set_score(email, quiz_id, score, achivable_score, needed_time):
 
 # GET SCORE
 def get_score(email, quiz_id):
+    # checks whether the email exists
     email_exists = check_if_email_exists(email)
     if email_exists["message_type"] == "Success":
+        # checks whether the quiz exists
         quiz_exists = check_if_quiz_exists(quiz_id)
         if quiz_exists["message_type"] == "Success":
+            # checks whether the score exists
             score_exists = check_if_score_exists(email, quiz_id)
             if score_exists["message_type"] == "Success":
+                # if so, the score is returned
                 score = dbc.get_score(email, quiz_id)
                 return score[0]
+            # if one of these criteria is not the case, a corresponding error message is returned
             return score_exists
         return quiz_exists
     return email_exists
@@ -421,8 +461,11 @@ def get_score(email, quiz_id):
 
 # GET ALL SCORES BY EMAIL
 def get_all_scores_by_email(email):
+    # checks whether the email exists
     email_exists = check_if_email_exists(email)
     if email_exists["message_type"] == "Success":
+        # if this is the case, all scores will be fetched from the database with this email and returned
         scores = dbc.get_all_scores_by_email(email)
         return scores
+    # if the email does not exist, a corresponding error message is returned
     return email_exists
